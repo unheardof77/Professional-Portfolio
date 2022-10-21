d95d6eae0a7121dfd0c3d06c89951f255d82c698
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-20 17:21:13 -0700

    All html documents can now direct to eachother.  Safe revert point.

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,5 +1,7 @@
 const router = require(`express`).Router();
 
-
+router.post(`/`, (req, res) =>{
+    
+})
 
 module.exports = router;
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require(`express`).Router();
 const apiRoutes = require(`./apiRoutes`);
 
-router.use(``, apiRoutes);
+router.use(`/email`, apiRoutes);
 
 module.exports = router;
