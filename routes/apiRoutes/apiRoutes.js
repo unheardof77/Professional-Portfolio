6be5b88c084b637f38043b4aff0f728ec0fea4be
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-21 12:38:38 -0700

    Worked on styling.

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -2,7 +2,6 @@ const router = require(`express`).Router();
 const path = require(`path`);
 router.post(`/`, (req, res) =>{
     console.log(req);
-    
     return res.sendFile(path.join(__dirname, `../../public/contactMe.html`));
 })
 
--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -4,15 +4,15 @@ const path = require(`path`);
 
 router.get(`/contactme`, (req, res) =>{
     res.sendFile(path.join(__dirname, `../../public/contactMe.html`))
-})
+});
 router.get(`/about`, (req, res) =>{
     res.sendFile(path.join(__dirname, `../../public/about.html`))
-})
+});
 router.get(`/projects`, (req, res) =>{
     res.sendFile(path.join(__dirname, `../../public/projects.html`))
-})
+});
 router.get(`/`, (req, res) =>{
     res.sendFile(path.join(__dirname, `../../public/home.html`))
-})
+});
 
 module.exports = router;
